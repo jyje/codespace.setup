e82c956501c82f0bf7b272b3f4ac0027c070ecd5
jyje <jyjeon@outlook.com>
2023-01-05 08:59:42 +0900

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+export INSTALL_ZSH=true
+export USERNAME=`jyje`
+
+apt update
+apt install zsh
+
+if [ "$INSTALL_ZSH" = "true" ]
+then
+    apt install -y \
+      zsh
+
+    cp -f ~/dotfiles/.zshrc ~/.zshrc
+    chsh -s /usr/bin/zsh $USERNAME
+    wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+    echo "source $PWD/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+fi
