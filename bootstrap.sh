f2366b6d1661b0c5d55a36bc93c0e531d1a9ecf3
jyje <jyjeon@outlook.com>
2023-01-05 09:41:19 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,21 @@
 export INSTALL_ZSH=true
-export USERNAME=`jyje`
+export USERNAME="jyje"
 
 echo "Bootstrapping for $USERNAME"
 
-apt update
-apt install zsh
+sudo apt update
 
 if [ "$INSTALL_ZSH" = "true" ]
 then
-    apt install -y \
-      zsh
+    sudo apt install -y zsh
 
     cp -f ~/dotfiles/.zshrc ~/.zshrc
     chsh -s /usr/bin/zsh $USERNAME
-    wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+    wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O - | zsh
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
     echo "source $PWD/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+    echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ${ZDOTDIR:-$HOME}/.zshrc
 fi
